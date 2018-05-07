435796e656a186fba051ce8d0cdcb18ac930ad71
rukiyeg <r.gungoroglu@gmail.com>
2018-05-07 15:48:38 +0200

    first commit

---
+++ src/main/java/com/formation/rkuloulou/App.java
@@ -0,0 +1,17 @@
+package com.formation.rkuloulou;
+
+public class App {
+
+	private final String message = "Hello c'est Rku et Loulou";
+
+	public App() {
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new App().getMessage());
+	}
+
+	private final String getMessage() {
+		return message;
+	}
+}
